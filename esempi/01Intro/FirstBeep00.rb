b506b3c1c1ed3a23e5cd87e84b85e8ac6d25fb0e
Leonardo Foletto <flttleonardo@gmail.com>
2021-05-30 13:40:11 +0200

    fix typo in first beep

--- esempi/01Intro/FirstBeep00.rb
+++ esempi/01Intro/FirstBeep00.rb
@@ -3,7 +3,7 @@
 
 play 60      # il comando play ci consente di definire una nota da suonare dando un valore in midi
 
-# l'asterisco indica un commento, prova a commentare linea 3 e uncommentare le linee 7,8,9
+# il simbolo # indica un commento, prova a commentare linea 3 e uncommentare le linee 8,9,10
 
 # play :c4    # play può usare come valore anche una nota midi nel formato qui dimostrato
 # sleep 1     # sleep ci consente di definire quanto tempo passerà tra un evento e il successivo
