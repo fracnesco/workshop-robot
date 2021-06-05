4dd67eaba609ae9fb5fcb93e4b7b14353090847a
Leonardo Foletto <flttleonardo@gmail.com>
2021-06-05 23:09:01 +0200

    es di funzione

---
+++ esempi/06Imperative_Prog/Funzioni.rb
@@ -0,0 +1,17 @@
+# Funzioni
+
+# Possiamo definire delle funzioni per aiutarci ad organizzare il codice nelle nostre composizioni
+
+var = 10
+
+define :func do     # define serve per definire una funzione di nome func
+  use_synth :dark_ambience
+  play var + 40, room: var * 3, detune1: var / 2, detune2: var * 2
+  sleep 1.33
+end
+
+
+5.times do          # suoniamo func 5 volte
+  func
+  var = var + 12    # ogni volta, dopo aver suonato func, assegniamo un nuovo valore a var
+end
