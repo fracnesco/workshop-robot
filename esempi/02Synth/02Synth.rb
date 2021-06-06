ef05400cc0ad0f95542a4b467eeda8a08668739a
NESSO <ffra237@gmail.com>
2021-06-06 16:37:25 +0200

    fixing typos and names

--- esempi/02Synth/02Synth.rb
+++ esempi/02Synth/02Synth.rb
@@ -1,6 +1,6 @@
 # Possiamo specificare dei parametri per ottenere più controllo sul suono
 
-3.times do                                  # questo comando vuol dire: fai quello che è contenuto in questo blocco epr 3 volte!
+3.times do                                  # questo comando vuol dire: fai quello che è contenuto in questo blocco per 3 volte!
   use_synth :prophet                        # in questo caso ho scelto di usare il synth prophet
   play :c5, cutoff: 60, res: 0.95           # posso controllare parametri aggiuntivi di un synth specificandoli dopo il messaggio play separati da una virgola
   sleep 0.25
